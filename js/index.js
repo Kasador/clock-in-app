c38e1d84018f6a270a3c72c7f454f200870132f5
Hunter Shaw <huntershaw23@gmail.com>
2019-05-19 21:11:17 -0600

    IN PROGRESS: Adding time stamp also clock-in button.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-$(document).ready({
-    
+$(document).ready(function(){
+    // toggle button
+    $('#clock-in').on('click', function(){
+        $(this).css('display', 'none');
+        $("#clock-out").fadeIn('slow');
+    });
+    $('#clock-out').on('click', function(){
+        $(this).css('display', 'none');
+        $("#clock-in").fadeIn('slow');
+    });
 });
